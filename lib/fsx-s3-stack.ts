30bcc10822a86ff22a8664d4921d387e7b179ced
Tim Cutts <tim@thecutts.org>
2025-02-01 22:08:46 +0000

    Revert to creating an example bucket

--- lib/fsx-s3-stack.ts
+++ lib/fsx-s3-stack.ts
@@ -1,12 +1,21 @@
-import { CfnOutput, CfnParameter, Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import {
+  CfnCondition,
+  CfnOutput,
+  CfnParameter,
+  Duration,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Asset } from "aws-cdk-lib/aws-s3-assets";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as fsx from "aws-cdk-lib/aws-fsx";
 import * as iam from "aws-cdk-lib/aws-iam";
-interface FsxS3StackProps extends StackProps {
-}
+
+interface FsxS3StackProps extends StackProps {}
+
 export class FsxS3Stack extends Stack {
   constructor(scope: Construct, id: string, props?: FsxS3StackProps) {
     super(scope, id, props);
@@ -18,30 +27,19 @@ export class FsxS3Stack extends Stack {
     const tieringConfiguration: s3.IntelligentTieringConfiguration = {
       name: "TierConfig",
       archiveAccessTierTime: Duration.days(90),
-      deepArchiveAccessTierTime: Duration.days(180)
-    }
+      deepArchiveAccessTierTime: Duration.days(180),
+    };
 
-    const bucketName = new CfnParameter(this, "BucketName", {
-      description: "Backing Bucket Name"
-    }).valueAsString
+    const bucket = new s3.Bucket(this, "BackingBucket", {
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      intelligentTieringConfigurations: [tieringConfiguration],
 
-    var bucket
-
-    if (bucketName != "") {
-      bucket = s3.Bucket.fromBucketName(this, "BackingBucket", bucketName)
-    } else {
-
-      bucket = new s3.Bucket(this, "BackingBucket", {
-        objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
-        intelligentTieringConfigurations: [tieringConfiguration],
-
-        // Do not use the following two lines in production
-        // better to import an existing bucket so that CloudFormation
-        // cannot accidentally destroy it
-        removalPolicy: RemovalPolicy.DESTROY,
-        autoDeleteObjects: true,
-      });
-    }
+      // Do not use the following two lines in production
+      // better to import an existing bucket so that CloudFormation
+      // cannot accidentally destroy it
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
     const lustrefs = new fsx.LustreFileSystem(this, "Lustre", {
       vpc: vpc,
@@ -56,19 +54,19 @@ export class FsxS3Stack extends Stack {
     });
 
     // Create a DRA between the filesystem and the bucket
-    const dra = new fsx.CfnDataRepositoryAssociation(this, 'DRA', {
+    const dra = new fsx.CfnDataRepositoryAssociation(this, "DRA", {
       dataRepositoryPath: `s3://${bucket.bucketName}/`,
       fileSystemId: lustrefs.fileSystemId,
-      fileSystemPath: '/',
+      fileSystemPath: "/",
       s3: {
         autoImportPolicy: {
-          events: ['NEW', 'CHANGED', 'DELETED'],
+          events: ["NEW", "CHANGED", "DELETED"],
         },
         autoExportPolicy: {
-          events: ['NEW', 'CHANGED', 'DELETED'],
-        }
-      }
-    })
+          events: ["NEW", "CHANGED", "DELETED"],
+        },
+      },
+    });
 
     const machineImage = new ec2.AmazonLinuxImage({
       generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
@@ -101,8 +99,8 @@ export class FsxS3Stack extends Stack {
     const mountPath = "/mnt/fsx";
     const dnsName = lustrefs.dnsName;
     const mountName = lustrefs.mountName;
-    var user = 'ec2-user';
-    var group = 'ec2-user';
+    var user = "ec2-user";
+    var group = "ec2-user";
 
     inst.userData.addCommands(
       "set -eux",
@@ -113,18 +111,18 @@ export class FsxS3Stack extends Stack {
       `chown ${user}:${group} ${mountPath}`,
       `echo "${dnsName}@tcp:/${mountName} ${mountPath} lustre defaults,noatime,flock,_netdev 0 0" >> /etc/fstab`,
       // Best practice settings for large HPC instances
-      "echo \"options ptlrpc ptlrpcd_per_cpt_max=32\" >> /etc/modprobe.d/modprobe.conf",
-      "echo \"options ksocklnd credits=2560\" >> /etc/modprobe.d/modprobe.conf",
+      'echo "options ptlrpc ptlrpcd_per_cpt_max=32" >> /etc/modprobe.d/modprobe.conf',
+      'echo "options ksocklnd credits=2560" >> /etc/modprobe.d/modprobe.conf',
       "reboot"
     );
 
     new CfnOutput(this, "InstanceID", {
       value: inst.instanceId,
-    })
+    });
 
-    new CfnOutput(this, 'FS mountname', {
+    new CfnOutput(this, "FS mountname", {
       value: mountName,
-    })
+    });
   }
 
   // best practices lustre settings for large HPC instances
@@ -147,13 +145,11 @@ export class FsxS3Stack extends Stack {
         new Asset(this, "lctl_settings.sh", {
           path: "assets/lctl_settings.sh",
         }),
-        { mode: "000700" },
+        { mode: "000700" }
       )
     );
 
-    const init = ec2.CloudFormationInit.fromElements(
-      ...localScripts,
-    );
+    const init = ec2.CloudFormationInit.fromElements(...localScripts);
 
     return init;
   }
